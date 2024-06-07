116b81094b4431f623aeb6184747a614d2f4a6fb
v.savchenko <v.savchenko@polytech.software>
2024-06-07 21:10:32 +0300

    3. Request Builder

--- test/pet.test.ts
+++ test/pet.test.ts
@@ -36,4 +36,48 @@ describe('User can get pet by', function () {
     assert(body.length > 0);
     assert(body.every((pet: any) => pet.tags.some((tag: any) => tag.name == 'tag1')));
   });
+
+  it('can be added, updated, deleted', async function () {
+    const petToCreate = {
+      category: {
+        id: 0,
+        name: 'string',
+      },
+      name: 'Doggo',
+      photoUrls: [
+        'https://img.freepik.com/premium-photo/cute-little-puppy-happy-dog_34683-4162.jpg',
+      ],
+      tags: [
+        {
+          id: 0,
+          name: 'string',
+        },
+      ],
+      status: 'available',
+    };
+
+    const addedPet = await pet.addNew(petToCreate);
+
+    assert.deepEqual(
+      addedPet,
+      {
+        ...petToCreate,
+        id: addedPet.id,
+      },
+      'Expect created pet'
+    );
+
+    const foundAddedPet = await pet.getById(addedPet.id);
+
+    assert.deepEqual(
+      foundAddedPet,
+      {
+        ...petToCreate,
+        id: addedPet.id,
+      },
+      'Expect created pet to be found'
+    );
+
+    await pet.delete(addedPet.id);
+  });
 });
